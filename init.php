e2bacb1626b530009d5ded079c059f967f1184d5
amol <amlo1232001@gmail.com>
2022-05-30 04:41:12 +0530

    as

--- init.php
+++ init.php
@@ -2,8 +2,8 @@
 session_start();
 require_once('config/config.php');
 require_once('config/db.php');
-// require_once('autoload.php');
-require('../vendor/autoload.php');
+require_once('autoload.php');
+// require('../vendor/autoload.php');
 if(isset($_SESSION['id'])){
   $name = $_SESSION['name'];
   $id = $_SESSION['id'];
