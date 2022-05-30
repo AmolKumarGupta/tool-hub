75e52440b3da3d4b3b68349b32d99c49ebfbb503
amol <amlo1232001@gmail.com>
2022-05-31 01:23:56 +0530

    [dev] deployment done

--- config/db.php
+++ config/db.php
@@ -1,11 +1,6 @@
 <?php
-// define('HOST','localhost');
-// define('USERNAME','root');
-// define('PASSWORD','');
-// define('DATABASE','toolhub');
-
-define('HOST','remotemysql.com');
-define('USERNAME','t5IRcSIzBd');
-define('PASSWORD','IdBH9npkSs');
-define('DATABASE','t5IRcSIzBd');
+define('HOST','localhost');
+define('USERNAME','root');
+define('PASSWORD','');
+define('DATABASE','toolhub');
 ?>
