d1ec464993c761bcbf9299676571b58abaa11e05
amol <amlo1232001@gmail.com>
2022-05-31 00:32:55 +0530

    h

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
 define('APPNAME','toolhub');
-define('PATH','/toolhub'); //if root directory is on the root then PATH should be a empty string
+define('PATH','/'); //if root directory is on the root then PATH should be a empty string
 define('LIMIT',8);
 ?>
